a45fa60241d57a6664d55eed3f3116952c8ddb68
Ziwara <161136425+Ziwara@users.noreply.github.com>
2025-02-10 00:35:07 +0800

    necessities

---
+++ index.js
@@ -0,0 +1,17 @@
+const messages = ["Are you sure?", "Really sure?", "Think again!", "Last chance!"];
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector(".no-button");
+    const yesButton = document.querySelector(".yes-button");
+    
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
